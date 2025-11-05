a959035b3cd2d4f8c858f1aa2af3eacddfc8515b
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-05 20:05:22 +0530

    asynchronous execution learn

--- lesson2-filesystem/appenddata.js
+++ lesson2-filesystem/appenddata.js
@@ -1,6 +1,7 @@
 import { appendFile } from 'node:fs';
+const path = require('node:path');
 
-appendFile('./Fileexample/appenddata.txt', 'hello world', (err) => {
+appendFile(path.join(__dirname , 'Fileexample', 'appenddata.txt'), 'hello world', (err) => {
   if (err) throw err;
   console.log('The "hello world" was appended to file!');
 });
--- lesson2-filesystem/writefile.js
+++ lesson2-filesystem/writefile.js
@@ -1,11 +1,21 @@
 const { writeFile } = require('node:fs/promises');
-const { resolve } = require('node:path');
+const { appendFile } = require('node:fs');
+const {rename} = require('node:fs');
+
 const path = require('node:path');
 async function WriteFile() {
   try {
-    const filePath = resolve(path.join(__dirname , 'Fileexample', 'writefile.txt'));
+    const filePath = (path.join(__dirname , 'Fileexample', 'writefile.txt'));
     await writeFile(filePath, 'Hello World', { encoding: 'utf8' });
-    console.log(`file written successfully`);
+    console.log(`written successfully`);
+    appendFile(path.join(__dirname , 'Fileexample', 'writefile.txt'), '\nhello world', (err) => {
+      if (err) throw err;
+      console.log('append completed!');
+      rename(path.join(__dirname , 'Fileexample', 'writefile.txt'), path.join(__dirname , 'Fileexample', 'afterwritefile.txt'), (err) => {
+        if (err) throw err;
+        console.log('rename completed!');
+      });
+    });
   } catch (err) {
     console.error(err.message);
   }
