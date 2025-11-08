d3f1e4e844764b608020af05f98d6ec156619d8f
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-08 19:58:12 +0530

    simple server creation

--- server.js
+++ server.js
@@ -1,51 +1,99 @@
-/* console.log("Server running on port 3000"); */
-//console.log(global);
-
-/* const os = require('os'); */
-
- /* console.log(os.type())
-console.log(os.version())
-console.log(os.homedir()) */
-
-/* console.log(__dirname)
-console.log(__filename) */
-
-/* const path = require('path')
-console.log(path.dirname(__filename))
-console.log(path.parse(__filename))
- */
-
-/* const math = require('./math') */
-//const {add,subtract,multiply,Division} = require('./math')
-/* console.log(math.add(4,5,2,7,2,8,1,99,88))
-console.log(math.subtract(4,5,2,7,2,8,1,99,88))
-console.log(math.multiply(4,5,2,7,2,8,1,99,88))
-console.log(math.Division(4,5,2,7,2,8,1,99,88))
- */
-
 const http = require('http');
+const path = require('path');
 const fs = require('fs');
-const PORT = 3000;
+const fsPromises = require('fs').promises;
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type' : 'text\html'});
-    fs.readFile('index.html' , (err , data) =>{
-        if(err){
-            res.writeHead(404);
-            res.write('Error : file not found');
-        }
-        else{
-            res.write(data);
-        }
-        res.end();
-    })
-})
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter { };
+// initialize object 
+const myEmitter = new Emitter();
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+const PORT = process.env.PORT || 3500;
 
-server.listen(PORT ,(error) =>{
-    if(error){
-        console.error(error.message);
+const serveFile = async (filePath, contentType, response) => {
+    try {
+        const rawData = await fsPromises.readFile(
+            filePath,
+            !contentType.includes('image') ? 'utf8' : ''
+        );
+        const data = contentType === 'application/json'
+            ? JSON.parse(rawData) : rawData;
+        response.writeHead(
+            filePath.includes('404.html') ? 404 : 200,
+            { 'Content-Type': contentType }
+        );
+        response.end(
+            contentType === 'application/json' ? JSON.stringify(data) : data
+        );
+    } catch (err) {
+        console.log(err);
+        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
+        response.statusCode = 500;
+        response.end();
     }
-    else{
-        console.log('server is running on port '+ PORT);
+}
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+
+    const extension = path.extname(req.url);
+
+    let contentType;
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
     }
-})
+
+    let filePath =
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
+
+    // makes .html extension not required in the browser
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+    const fileExists = fs.existsSync(filePath);
+
+    if (fileExists) {
+        serveFile(filePath, contentType, res);
+    } else {
+        switch (path.parse(filePath).base) {
+            case 'old-page.html':
+                res.writeHead(301, { 'Location': '/new-page.html' });
+                res.end();
+                break;
+            case 'www-page.html':
+                res.writeHead(301, { 'Location': '/' });
+                res.end();
+                break;
+            default:
+                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+        }
+    }
+});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
