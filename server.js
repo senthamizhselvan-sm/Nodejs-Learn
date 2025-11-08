1cd5c0af28e7a1c340e957bd367783f0d574c580
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-08 19:23:51 +0530

    logevents updates

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-console.log("Server running on port 3000");
+/* console.log("Server running on port 3000"); */
 //console.log(global);
 
-const os = require('os');
+/* const os = require('os'); */
 
  /* console.log(os.type())
 console.log(os.version())
@@ -15,9 +15,37 @@ console.log(path.dirname(__filename))
 console.log(path.parse(__filename))
  */
 
-const math = require('./math')
+/* const math = require('./math') */
 //const {add,subtract,multiply,Division} = require('./math')
-console.log(math.add(4,5,2,7,2,8,1,99,88))
+/* console.log(math.add(4,5,2,7,2,8,1,99,88))
 console.log(math.subtract(4,5,2,7,2,8,1,99,88))
 console.log(math.multiply(4,5,2,7,2,8,1,99,88))
 console.log(math.Division(4,5,2,7,2,8,1,99,88))
+ */
+
+const http = require('http');
+const fs = require('fs');
+const PORT = 3000;
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'content-type' : 'text\html'});
+    fs.readFile('index.html' , (err , data) =>{
+        if(err){
+            res.writeHead(404);
+            res.write('Error : file not found');
+        }
+        else{
+            res.write(data);
+        }
+        res.end();
+    })
+})
+
+server.listen(PORT ,(error) =>{
+    if(error){
+        console.error(error.message);
+    }
+    else{
+        console.log('server is running on port '+ PORT);
+    }
+})
